1d176accb1f336a9eaaf235b1976810a01d90ef0
Chandan Saurav Panda <chandan.saurav.panda@sap.com>
2024-01-27 14:11:47 +0000

    Product plant table corrected

--- frontend/src/components/ProdPlantTable.js
+++ frontend/src/components/ProdPlantTable.js
@@ -6,49 +6,45 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
-function createData(
-    name,
-    calories,
-    fat,
-    carbs,
-    protein
-  ) {
-    return { name, calories, fat, carbs, protein };
+  const getData = async () => {
+
+    try {
+    const response = await axios.get("/api/ProductPlant_Listing",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": "Fetch"
+      }
+    });
+    const data = response.data;
+    console.log("result",data);
+    return data.value;
+  } catch (error) {
+    console.log(error);
   }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
+};
+
+
 
 const ProdPlantTable = () => {
 
+  // Get the data from server
+  const [data, setData] = useState([]);
+  useEffect(() => {
 
-  //Get the csrfToken from Approuter
-  axios.get("/api/ProductPlant_Listing",
-  {
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRF-Token": "Fetch"
-    }
-  }).then((response) => {
-
-    console.log(response.data);
-    // let csrfToken = response.headers['x-csrf-token'];
-    // //Call POST with received csrfToken
-    // axios.post( "/api/triggerNotification",
-    // { }, {
-    //   headers: {
-    //     'X-CSRF-Token': csrfToken
-    //   }
-    // }
-    // ).catch((error) => {console.log(error);});
-
-  }).catch((error) => {console.log(error);});
+    const fetchData = async () => {
+      try {
+        const result = await getData();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  },[]);
 
 return(
 
@@ -56,26 +52,22 @@ return(
     <Table sx={{ minWidth: 650 }} aria-label="simple table">
       <TableHead>
         <TableRow>
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          <TableCell>Product</TableCell>
+          <TableCell>Plant</TableCell>
+          <TableCell align="right">isListed</TableCell>
         </TableRow>
       </TableHead>
       <TableBody>
-        {rows.map((row) => (
+        {data.map((line) => (
           <TableRow
-            key={row.name}
+            key={`${line.product_id}-${line.plant_id}`}
             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
           >
-            <TableCell component="th" scope="row">
-              {row.name}
+            <TableCell component="th" scope="line">
+              {line.product_id}
             </TableCell>
-            <TableCell align="right">{row.calories}</TableCell>
-            <TableCell align="right">{row.fat}</TableCell>
-            <TableCell align="right">{row.carbs}</TableCell>
-            <TableCell align="right">{row.protein}</TableCell>
+            <TableCell>{line.plant_id}</TableCell>
+            <TableCell align="right">{line.isListed? "Yes": "No"}</TableCell>
           </TableRow>
         ))}
       </TableBody>
@@ -86,3 +78,5 @@ return(
 }
 
 export default ProdPlantTable;
+
+
